2baf86afc1a229ef0a67a2cb05e84c1df45d12fb
Savaş Dersim Çelik <33200780+savasdersimcelik@users.noreply.github.com>
2020-10-26 20:53:26 +0300

    First

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# bootstrap-modal-image-picker
- Wordpress Tarzında Resim Seçme Sistemi
+Projelerinize wordpress tarzında bir resim seçici eklemek isterseniz kullanabileceğiniz sistem.
+
+![alt text](https://github.com/[username]/[reponame]/blob/[branch]/image.jpg?raw=true)
--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Optional code
 
 // Tile Picker Active Border
-$(document).on("click", ".tile-picker input", function(e) {
-  $('.wall').find('.tile-picker').removeClass("active");
-  $(this).closest(".tile-picker").addClass("active");
+$(document).on("click", ".sdc-picker input", function(e) {
+  $('.wall').find('.sdc-picker').removeClass("active");
+  $(this).closest(".sdc-picker").addClass("active");
 });
 
-$(document).on("click", ".modal-image-select", function(e) {
+$(document).on("click", ".sdc-modal-image-select", function(e) {
   $('.wall').find('.active').each(function( index, element ) {
     $('.select-image-src').attr('src', $(element).data('src'));
     $('.select-image-id').val($(element).data('id'));
   });
-  $('.wall').find('.tile-picker').removeClass("active");
+  $('.wall').find('.sdc-picker').removeClass("active");
 });
 
-$(document).on("click", ".modal-cancel-select", function(e) {
-  $('.wall').find('.tile-picker').removeClass("active");
+$(document).on("click", ".sdc-modal-cancel-select", function(e) {
+  $('.wall').find('.sdc-picker').removeClass("active");
 });
